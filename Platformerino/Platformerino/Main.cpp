6a5682d16b42758a36d3d959c8be8a8be21a64f4
Tyzlous <rockingrobin.frank@gmail.com>
2018-11-01 02:07:04 +0100

    Testcube movement

--- Platformerino/Platformerino/Main.cpp
+++ Platformerino/Platformerino/Main.cpp
@@ -3,9 +3,11 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(900, 900), "SFML works!");
 	window.setVerticalSyncEnabled(true);
-	sf::CircleShape shape(100.f);
+	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+	sf::RectangleShape shape(sf::Vector2f(100.0f, 100.0f));
+	shape.setPosition(sf::Vector2f(200.0f, 200.0f));
 	shape.setFillColor(sf::Color::Green);
 
 	while (window.isOpen())
@@ -15,9 +17,30 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+				{
+					player.move(sf::Vector2f(0.0f, -0.3f));
+				}
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+				{
+					player.move(sf::Vector2f(-0.3f, 0.00f));
+				}
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+				{
+					player.move(sf::Vector2f(0.0f, 0.3f));
+				}
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+				{
+					player.move(sf::Vector2f(0.3f, 0.0f));
+				}
+			}
 		}
 
 		window.clear();
+		window.draw(player);
 		window.draw(shape);
 		window.display();
 	}
